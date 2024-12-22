7f081c4c186a0246b166e5408004338a05c1b7bc
carinaluise <carinalrobinson@gmail.com>
2024-12-22 19:45:24 +0000

    Clean up routes config file

--- config/routes.rb
+++ config/routes.rb
@@ -13,17 +13,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  
-end
 
-Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
-  resources :products
   root "products#index"
   resources :products do
     resources :subscribers, only: [ :create ]
   end
   
 end
+
 
