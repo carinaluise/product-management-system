2dc059c0f825748b8882ca4b8dbb43b1a11109c7
carinaluise <carinalrobinson@gmail.com>
2024-12-22 22:21:16 +0000

    Fix Mailer text using routes url helper and set up method

--- test/mailers/product_mailer_test.rb
+++ test/mailers/product_mailer_test.rb
@@ -1,11 +1,23 @@
 require "test_helper"
 
 class ProductMailerTest < ActionMailer::TestCase
+  include Rails.application.routes.url_helpers
+
+  setup do
+    @product = products(:one)
+    @subscriber = subscribers(:one)
+    @host = "example.com"
+    Rails.application.routes.default_url_options[:host] = @host
+  end
+
   test "in_stock" do
-    mail = ProductMailer.in_stock
-    assert_equal "In stock", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = ProductMailer.with(product: @product, subscriber: @subscriber).in_stock
+
+    assert_equal "Product Back in Stock", mail.subject
+    assert_equal [@subscriber.email], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Good news!", mail.body.encoded
+    assert_match "#{@product.name} is back in stock.", mail.body.encoded
+    assert_match product_url(@product, host: @host), mail.body.encoded
   end
 end
