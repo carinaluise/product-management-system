8b4105329cd07158d2e68bcbdc2e49516a5aa869
carinaluise <carinalrobinson@gmail.com>
2024-12-22 22:27:10 +0000

    Run rubocop

--- test/mailers/product_mailer_test.rb
+++ test/mailers/product_mailer_test.rb
@@ -14,8 +14,8 @@ class ProductMailerTest < ActionMailer::TestCase
     mail = ProductMailer.with(product: @product, subscriber: @subscriber).in_stock
 
     assert_equal "Product Back in Stock", mail.subject
-    assert_equal [@subscriber.email], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal [ @subscriber.email ], mail.to
+    assert_equal [ "from@example.com" ], mail.from
     assert_match "Good news!", mail.body.encoded
     assert_match "#{@product.name} is back in stock.", mail.body.encoded
     assert_match product_url(@product, host: @host), mail.body.encoded
