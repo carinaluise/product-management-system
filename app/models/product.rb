8f98ac2dc2c0d88a90f7a22f3a1cf8647e137c35
carinaluise <carinalrobinson@gmail.com>
2024-12-24 20:25:49 +0000

    Allow null for inventory count

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,5 @@ class Product < ApplicationRecord
     has_one_attached :featured_image
     has_rich_text :description
     validates :name, presence: true
-    validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
+    validates :inventory_count, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
 end
