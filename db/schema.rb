db648a6cd7e2866c80d2bd68d2782e515312a10c
carinaluise <carinalrobinson@gmail.com>
2024-12-26 20:49:33 +0000

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_22_192543) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_26_202635) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_22_192543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "inventory_count"
+    t.decimal "price"
   end
 
   create_table "sessions", force: :cascade do |t|
