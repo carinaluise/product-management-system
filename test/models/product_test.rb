01c8007d0d3140284a7048b428e3d3dfa0d35949
carinaluise <carinalrobinson@gmail.com>
2024-12-26 22:55:24 +0000

    Use fixture in product test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,14 +3,17 @@ require "test_helper"
 class ProductTest < ActiveSupport::TestCase
   include ActionMailer::TestHelper
 
+  setup do
+    @product = products(:tshirt)
+  end
+
   test "sends email notifications when back in stock" do
-    product = products(:tshirt)
-
     # Set product out of stock
-    product.update(inventory_count: 0)
+    @product.update!(inventory_count: 0)
 
+    # Set product back in stock and check for emails
     assert_emails 2 do
-      product.update(inventory_count: 99)
+      @product.update!(inventory_count: 99)
     end
   end
 end
