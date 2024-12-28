f521de4cdf4535eefa36797d5c287b7d91842bd1
carinaluise <carinalrobinson@gmail.com>
2024-12-28 18:51:59 +0000

    Extend tailwind theme with custom colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,7 +8,16 @@ module.exports = {
     "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          100: "#928dab",
+          200: "#1f1c2c",
+          300: "rgba(31, 28, 44, 0.7)",
+          400: "rgba(146, 141, 171, 0.7)",
+        },
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
